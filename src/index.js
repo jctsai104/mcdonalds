6c1abbca3fe98cf60b00ef5fdc570fca092fcb92
jctsai104 <alice60313@hotmail.com>
2022-04-23 23:36:27 +0800

    大概定位 活動卡片的部分加入vue的指令

---
+++ src/index.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#app',
+    data: {
+        cards:[
+            {
+                id: 1,
+                img: "https://www.mcdonalds.com/is/image/content/dam/tw/featured-callout/desktop/zh-tw/what-hot-PC.jpg?$FC_One_By_Two$",
+                title: "現正推出",
+                content: "想了解麥當勞有哪些最新活動嗎？你不想錯過的美食、最即時的優惠內容... 馬上探索、馬上行動！",
+
+            }
+        ],
+    },
+})
